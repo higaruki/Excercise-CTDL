7da48727250fcc283437d3ae061038cb4f5af9d0
Hjgark <luatdell3537@gmail.com>
2023-10-07 20:17:42 +0700

    Update Order.java

--- Lab3/src/Task2/Order.java
+++ Lab3/src/Task2/Order.java
@@ -3,10 +3,10 @@ package Task2;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class Order {
+public class Order2 {
 	private OrderItem[] items;
 
-	public Order(OrderItem[] items) {
+	public Order2(OrderItem[] items) {
 		super();
 		this.items = items;
 	}
@@ -19,36 +19,47 @@ public class Order {
 		return cost;
 	}
 
-	public OrderItem[] sort(OrderItem[] items) {
-		Arrays.sort(items, Comparator.comparing(item -> item.getProduct().getPrice()));
-		return items;
-	}
-
 	// using binary search approach
 	public boolean contains(Product p) {
-		OrderItem[] newItems = sort(items);
-		int mid = newItems.length / 2;
+		OrderItem[] newItem = sort(items);
+		int mid = newItem.length / 2;
 		int left = 0;
-		int right = newItems.length - 1;
+		int right = newItem.length - 1;
 		while (left < right) {
-			if (p.getPrice() > newItems[mid].getPrice()) {
+			if (p.getPrice() > newItem[mid].getPrice()) {
 				left = mid + 1;
-				mid = newItems.length - 1 - ((right - left) / 2);
+				mid = newItem.length - 1 - ((right - left) / 2);
 			}
-			if (p.getPrice() < newItems[mid].getPrice()) {
+			if (p.getPrice() < newItem[mid].getPrice()) {
 				right = mid - 1;
 				mid = (right - left) / 2;
 			}
-			if (p.getPrice() == newItems[mid].getPrice()) {
+			if (p.getPrice() == newItem[mid].getPrice()) {
 				return true;
 			}
-			if (right == left && p.getPrice() == newItems[mid].getPrice()) {
+			if (right == left && p.getPrice() == newItem[mid].getPrice()) {
 				return true;
 			}
 		}
 		return false;
 	}
-
+	public boolean contains2(Product p) {
+		OrderItem[] newItem = sort(items);
+		int left = 0;
+		int right = newItem.length - 1;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			int comparison = newItem[mid].getProduct().getPrice().compareTo(p.getPrice());
+			if (comparison == 0) {
+				return true;
+			} else if (comparison < 0) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		return false;
+	}
 	// get all products based on the given type using linear search
 	public int getLength(OrderItem[] items, String type) {
 		int count = 0;
@@ -71,6 +82,11 @@ public class Order {
 		return correctOnType;
 	}
 
+	public OrderItem[] sort(OrderItem[] items) {
+		Arrays.sort(items, Comparator.comparing(item -> item.getProduct().getPrice()));
+		return items;
+	}
+
 	public static void main(String[] args) {
 		Product p1 = new Product("01", "Coca", 10000, "Drink");
 		Product p2 = new Product("02", "Pepsi", 9000, "Drink");
@@ -81,6 +97,7 @@ public class Order {
 		Product p7 = new Product("07", "Spagetti", 50000, "Food");
 		Product p8 = new Product("08", "Hot Dog", 30000, "Food");
 		Product p9 = new Product("09", "Cheese Burger", 35000, "Food");
+		Product p10 = new Product("10s", "Cheese Noodles", 45000, "Food");
 		OrderItem oi1 = new OrderItem(p1, 50);
 		OrderItem oi2 = new OrderItem(p2, 30);
 		OrderItem oi3 = new OrderItem(p3, 20);
@@ -90,22 +107,13 @@ public class Order {
 		OrderItem oi7 = new OrderItem(p7, 70);
 		OrderItem oi8 = new OrderItem(p8, 40);
 		OrderItem oi9 = new OrderItem(p9, 40);
+		OrderItem oi10 = new OrderItem(p10, 40);
 		OrderItem[] items = { oi1, oi2, oi3, oi4, oi5, oi6, oi7, oi8 };
 		Order o1 = new Order(items);
-		// cost
-		System.out.println("cost : " + o1.cost());
-		// filter
-		System.out.println("filter : ");
-		System.out.println(Arrays.toString(o1.filter("Food")));
-		System.out.println(Arrays.toString(o1.filter("Drink")));
-		// sort based on Price
-		System.out.println("sort : ");
-		System.out.println(Arrays.toString(o1.sort(items)));
-//		 contain
-		System.out.println("contains : ");
-		// contains() bị lỗi khi Product là p1 và p6
-		// vẫn chưa fix được
-		System.out.println(o1.contains(p4));
-		System.out.println(o1.contains(p9));
+//		System.out.println(o1.cost());
+//		System.out.println(Arrays.toString(o1.filter("Food")));
+//		System.out.println(Arrays.toString(o1.sort(items)));
+		System.out.println(o1.contains(p1));
+//		System.out.println(o1.contains(p10));
 	}
 }
